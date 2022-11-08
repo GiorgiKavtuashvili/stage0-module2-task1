1cdf130a3e57282369980d5fb6d7eb85d78074ba
Giorgi <giorgi_kavtuashvili@yahoo.com>
2022-11-08 21:22:12 +0400

    Stage0 module3 task1 Is Done

---
+++ src/main/java/com/mjc/stage0/BrokedApp.java
@@ -0,0 +1,8 @@
+package com.mjc.stage0;
+
+public class BrokedApp {
+    public static void main(String[] args)  {
+        System.out.println("Fix" + "\n" + "me");
+
+    }
+}
---
+++ src/main/java/com/mjc/stage0/PrintSentenceApp.java
@@ -0,0 +1,8 @@
+package com.mjc.stage0;
+
+public class PrintSentenceApp {
+    public static void main(String[] args)  {
+        //This is my first Java program
+        System.out.println("This is my first Java program");
+    }
+}
